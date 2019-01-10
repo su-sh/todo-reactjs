cc11e3a60ca222f5188eff9741700a671a03aa52
Sushant Shrestha <yala.sushantshrestha@gmail.com>
2019-01-10 10:14:27 +0545

    FirstCommit: Todo Add, Complete, Delete Features added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Home from './component/Home';
+import Completed from './component/Completed';
+import Remaining from './component/Remaining';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: [
+        {
+          id: 123,
+          content: 'create react app',
+          completed: false
+        },
+        {
+          id: 124,
+          content: 'create java app',
+          completed: true
+        }
+      ],
+      currentView: 'home'
+    };
+  }
+
   render() {
+    let currentView = this.state.currentView;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {/* Header */}
+        <div>
+          {/* tabs */}
+          <div>
+            <ul>
+              <li onClick={() => this.setState({ currentView: 'home' })}>
+                Home
+              </li>
+              <li onClick={() => this.setState({ currentView: 'completed' })}>
+                Completed
+              </li>
+              <li onClick={() => this.setState({ currentView: 'remaining' })}>
+                Remaining
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Body */}
+        {currentView === 'home' && (
+          <Home
+            addTodoItem={this.addTodoItem}
+            todoList={this.state.todoList}
+            toggleTodoItemCompleted={this.toggleTodoItemCompleted}
+            deleteTodoItem={this.deleteTodoItem}
+          />
+        )}
+        {currentView === 'remaining' && <Remaining />}
+        {currentView === 'completed' && <Completed />}
       </div>
     );
   }
+
+  addTodoItem = todoContent => {
+    let todoList = this.state.todoList,
+      newId = Date.now(),
+      todo = {
+        id: newId,
+        content: todoContent,
+        completed: false
+      };
+
+    todoList.push(todo);
+
+    this.setState(todoList);
+  };
+
+  deleteTodoItem = id => {
+    const todoList = this.state.todoList.filter(todoItem => {
+      if (todoItem.id !== id) {
+        return todoItem;
+      }
+
+      return null;
+    });
+
+    this.setState({ todoList });
+  };
+
+  toggleTodoItemCompleted = id => {
+    const todoList = this.state.todoList.filter(todoItem => {
+      if (todoItem.id === id) {
+        todoItem.completed = !todoItem.completed;
+      }
+
+      return todoItem;
+    });
+
+    this.setState({ todoList });
+  };
 }
 
 export default App;
---
+++ src/component/Remaining.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Remaining = () => {
+  return <div>Remaining</div>;
+};
+
+export default Remaining;
