05256bb2b93df89488f87679c0a7f1a3246058ec
Sushant Shrestha <yala.sushantshrestha@gmail.com>
2019-01-16 09:52:54 +0545

    Updates

---
+++ precache-manifest.51a56e6d925707e5becd990382222185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "07ff080034ca661787bb92b8e4c35436",
+    "url": "/todo-reactjs/static/media/logo.07ff0800.png"
+  },
+  {
+    "revision": "24f435695d05cf7cb8adcbc74d1c7e0c",
+    "url": "/todo-reactjs/static/media/add.24f43569.png"
+  },
+  {
+    "revision": "2856e2b83d2fe1b765e7",
+    "url": "/todo-reactjs/static/js/runtime~main.2856e2b8.js"
+  },
+  {
+    "revision": "038c6cae44019cd7efc0",
+    "url": "/todo-reactjs/static/js/main.038c6cae.chunk.js"
+  },
+  {
+    "revision": "399e71ad78c00a0b7c51",
+    "url": "/todo-reactjs/static/js/1.399e71ad.chunk.js"
+  },
+  {
+    "revision": "038c6cae44019cd7efc0",
+    "url": "/todo-reactjs/static/css/main.c2aaeebf.chunk.css"
+  },
+  {
+    "revision": "1f11e26261f0137bb5d467813fe4b81a",
+    "url": "/todo-reactjs/index.html"
+  }
+];
